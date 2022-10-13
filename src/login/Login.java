9f2a72886db2f96ed3d4e72bf8b972b433fd6098
abindran <abindranr@gmail.com>
2022-10-13 07:43:35 +0530

    final

--- src/login/Login.java
+++ src/login/Login.java
@@ -1,8 +1,20 @@
 package login;
 
+import database.ConnectionMySql;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.text.html.ImageView;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Login extends JFrame implements ActionListener {
 
@@ -11,40 +23,63 @@ public class Login extends JFrame implements ActionListener {
     JPasswordField passwordField;
     JButton login,signup;
     JLabel username,password;
+    JLabel imageLabel;
 
-    Login(){
+    Login() throws IOException {
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
-        this.setSize(640,360);
-        this.setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setSize(640,460);
+        setResizable(false);
+
+        getContentPane().setBackground(new Color(0x242B2E));
+
 
         username = new JLabel("Username: ");
-        username.setBounds(160,100,75,25);
+        username.setBounds(160,210,75,25);
+        username.setForeground(Color.WHITE);
 
         password = new JLabel("Password: ");
-        password.setBounds(160,150,75,25);
+        password.setBounds(160,260,75,25);
+        password.setForeground(Color.WHITE);
 
         usernameField = new JTextField();
-        usernameField.setBounds(245,100,200,25);
+        usernameField.setBounds(245,210,200,25);
 
         passwordField = new JPasswordField();
-        passwordField.setBounds(245,150,200,25);
+        passwordField.setBounds(245,260,200,25);
 
         login = new JButton("Login");
-        login.setBounds(240,200,100,25);
+        login.setBounds(240,310,100,25);
         login.addActionListener(this);
 
         signup = new JButton("SignUp");
-        signup.setBounds(350,200,100,25);
+        signup.setBounds(350,310,100,25);
         signup.addActionListener(this);
 
+        imageLabel = new JLabel();
+        BufferedImage img = ImageIO.read(new File("D:\\IPTV\\src\\resources\\login-i.png"));
+        ImageIcon loginImage = new ImageIcon(img);
+        imageLabel.setIcon(loginImage);
+        imageLabel.setBackground(new Color(0x242B2E));
+        setForeground(new Color(0x242B2E));
+        imageLabel.setOpaque(false);
+        imageLabel.setBounds(200,15,400,177);
+
+
+        setLocation(300,300);
+        setTitle("STB Player-Login");
+
+        this.add(imageLabel);
         this.add(username);
         this.add(password);
         this.add(usernameField);
         this.add(passwordField);
         this.add(login);
         this.add(signup);
+
+        ImageIcon icon = new ImageIcon("D:\\IPTV\\src\\resources\\logo.png");
+        setIconImage(icon.getImage());
         this.setVisible(true);
 
 
@@ -53,18 +88,52 @@ public class Login extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        // Event handling for login button
         if(e.getSource() == login){
             String username = usernameField.getText();
             String password = String.valueOf(passwordField.getPassword());
+            Boolean isLoggedIn = false;
 
-        }else if(e.getSource() == signup){
+            try {
+                Connection dbCon = ConnectionMySql.getConnection();
+                Statement smt = dbCon.createStatement();
+                String loginQuery = "SELECT username,password FROM users";
+                ResultSet rs = smt.executeQuery(loginQuery);
+
+
+                while (rs.next()) {
+                     isLoggedIn =  rs.getString("username").equals(username) &&
+                                          rs.getString("password").equals(password);
+                    if(isLoggedIn) {
+                        // TODO: Direct to home page and dispose the current window
+                    }
+                }
+
+                dbCon.close();
+
+            } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+
+            if(!isLoggedIn){
+                JOptionPane.showInternalMessageDialog(null,"Enter correct username or password !!!","Invalid Credentials !!!",JOptionPane.ERROR_MESSAGE);
+            }
+
+        }
+
+        // Event handling for signup button
+        if(e.getSource() == signup){
             dispose();
             new SignUp();
         }
 
     }
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
         Login loginTest = new Login();
     }
 }
